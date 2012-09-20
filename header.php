fdcd56a0626dc0045d2717ff500507522b9d0951
Jeremy Boggs <jeremy@clioweb.org>
2012-09-20 14:13:38 -0400

    Updates javascripts.
    * Removes selectivizr from the Modernizr mq check.
    * Adds call to jQuery from the Google CDN.
    * Adds selectivizr in an IE conditional.

--- header.php
+++ header.php
@@ -5,18 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo bsuva_site_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-    <!-- Modernizr and Friends -->
+
+    <!-- JavaScripts -->
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/javascripts/modernizr.min.js"></script>
     <script>
       Modernizr.load([
         {
           test: Modernizr.mq(),
-          nope: ['<?php echo get_stylesheet_directory_uri(); ?>/javascripts/respond.min.js',
-          '<?php echo get_stylesheet_directory_uri(); ?>/javascripts/selectivizr.min.js']
+          nope: '<?php echo get_stylesheet_directory_uri(); ?>/javascripts/respond.min.js'
         }
       ]);
     </script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+    <!--[if (gte IE 6)&(lte IE 8)]>
+    <script src="<?php echo get_template_directory_uri(); ?>/javascripts/selectivizr.min.js"></script>
+    <![endif]-->
+
     <?php wp_head(); ?>
+
 </head>
 <body <?php body_class(); ?>>
     <header role="banner">
